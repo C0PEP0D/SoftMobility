9688fd405e7b6a3c8b6a6dce6db11a60a9d72427
celoy <eloy@irphe.univ-mrs.fr>
2026-02-19 12:08:26 +0100

    time integration updated

--- softmobility/__init__.py
+++ softmobility/__init__.py
@@ -4,6 +4,7 @@ from .classes.sphere import Sphere
 from .classes.sphereassembly import SphereAssembly
 from .classes.softbody import SoftBody
 from .classes.flow import Flow
+from .classes.field import Field
 from .classes.flowbody import FlowBody
 
 from ._version import get_versions
--- softmobility/classes/flowbody.py
+++ softmobility/classes/flowbody.py
@@ -1,185 +1,190 @@
+"""Class FlowBody used to integrate the trajectory of a soft body in a flow, subject to a field"""
+
 import jax.numpy as jnp
 import jax
 from jax import lax
 
-from softmobility import SoftBody, Flow
+from softmobility import SoftBody, Flow, Field
 
 
 class FlowBody:
+    """
+    Solver for fluid-structure interaction.
+
+    Parameters:
+    - soft_plankton: SoftBody object
+    - flow: A Flow object
+    - init_position: a 3D list or array (default [0, 0, 0])
+    - init_orientation: a 3D list or array (default [0, 0, 0])
+    - dt: Time step for integration (default 0.01)
+    - integrator: str, one of: "Euler", "RK2", or "RK4" (default "RK2")
+    """
+
     def __init__(
         self,
         soft_body: SoftBody,
         flow: Flow,
+        field: Field,
         init_position=[0, 0, 0],
         init_orientation=[0, 0, 0],
         dt=0.01,
         integrator="RK2",
     ):
-        """
-        Solver for fluid-structure interaction.
-
-        Parameters:
-        - soft_plankton: SoftBody object
-        - flow: A Flow object
-        - init_position: a 3D list or array (default [0, 0, 0])
-        - init_orientation: a 3D list or array (default [0, 0, 0])
-        - dt: Time step for integration (default 0.01)
-        - integrator: str, one of: "Euler", "RK2", or "RK4" (default "RK2")
-        """
-        self.soft_plankton = soft_body
+        self.soft_body = soft_body
         self.flow = flow
-        self.dof = self.soft_plankton.dof_defaults  # Degrees of freedom
+        self.field = field
+        self.dofs = self.soft_body.dof_defaults  # Degrees of freedom
+        self.inputs = self.soft_body.input_defaults
         self.position = jnp.array(init_position)
         self.orientation = jnp.array(init_orientation)
         self.dt = dt
         self.integrator = integrator
-        self.trajectory = [[self.dof, self.position, self.orientation]]
+        self.trajectory = [[self.position, self.orientation, self.dofs]]
         self.time = 0.0
         self.compute_fast_mobility = jax.jit(self._compute_fast_mobility)
         self.compute_grand_velocity = jax.jit(self._compute_grand_velocity)
 
-    def _compute_fast_mobility(self, dofs):
+    def _compute_fast_mobility(self, dofs_inputs):
         """JIT-compiled function for computing mobility."""
-        params = self.soft_plankton.param_defaults
-        return self.soft_plankton.compute_mobility_problem(dofs, params)
+        dofs = dofs_inputs[: self.soft_body.Ndof]
+        design = self.soft_body.design_defaults
+        inputs = dofs_inputs[self.soft_body.Ndof :]
+        return self.soft_body.compute_mobility_problem(dofs, design, inputs)
 
     def grand_velocity(self):
-        """Compute grand velocity of the plankton."""
+        """Compute grand velocity of the body."""
         # Extract state variables
         position = self.position
         orientation = self.orientation
-        dof = self.dof
+        dofs = self.dofs
+        inputs = self.inputs
+        time = self.time
 
-        # Compute the flow at plankton position (in the lab framework)
-        v_lab = self.flow.velocity(position)
+        # Compute the flow at body position (in the lab framework)
+        u_lab = self.flow.velocity(position)
         omega_lab, s_lab = self.flow.omega_s(position)
 
-        return self.compute_grand_velocity(orientation, dof, v_lab, omega_lab, s_lab)
+        # Commpute the field value at body position (in the lab framework)
+        field_lab = self.field.vector(position, time)
 
-    def _compute_grand_velocity(self, orientation, dof, v_lab, omega_lab, s_lab):
+        return self.compute_grand_velocity(orientation, dofs, inputs, u_lab, omega_lab, s_lab, field_lab)
+
+    def _compute_grand_velocity(self, orientation, dofs, inputs, u_lab, omega_lab, s_lab, field_lab):
         # Compute rotation matrices
-        R, grand_R = rotation_matrix_from_Rodrigues(orientation, Ndof=self.soft_plankton.Ndof)
+        R, sixc_R = rotation_matrix_from_Rodrigues(orientation, Ndof=self.soft_body.Ndof)
 
-        # Compute flow velocities in the particle framework
-        v_inf = R.T @ v_lab
-        omega_inf = R.T @ omega_lab
-        u_inf = jnp.block([v_inf, omega_inf])
+        # Compute flow velocities and field value in the body's framework
+        p_lab = jnp.block([u_lab, omega_lab, jnp.zeros(self.soft_body.Ndof)])
+        field = R.T @ field_lab
 
         # Compute rate-of-strain in the particle framework
         s_part = R.T @ s_lab @ R
         s_inf = jnp.array([s_part[0, 0], s_part[0, 1], s_part[0, 2], s_part[1, 1], s_part[1, 2]])
 
-        matrices = self.compute_fast_mobility(dof)
-        Mtilde = matrices.Mtilde
-        Mm = matrices.Mm
-        G = matrices.G
-        V = matrices.V
+        tensors = self.compute_fast_mobility(jnp.block([dofs, inputs]))
+        M_H = tensors.M_H
+        M_K = tensors.M_K
+        C_E = tensors.C_E
 
-        # Extract elastic forces/torques
-        F_elastic = self.soft_plankton.grand_forces_func(dofs=dof)
-
-        # Compute soft mobility equation
-        gravity = jnp.array([0, 0, -1])
-        grand_velocity = Mtilde @ F_elastic + Mm @ gravity + V @ u_inf + G @ s_inf
+        sixc_velocity = M_H @ field + M_K @ dofs + C_E @ s_inf
 
         # Compute velocities in lab framework
-        grand_velocity_lab = grand_R @ grand_velocity
+        sixc_velocity_lab = sixc_R @ sixc_velocity + p_lab
 
-        return grand_velocity_lab
+        return sixc_velocity_lab
 
     def integrate_euler(self):
         """Euler first-order integration."""
 
         Qdot = self.grand_velocity()
 
-        self.dof += self.dt * Qdot[:-6]
-        self.position += self.dt * Qdot[-6:-3]
-        self.orientation += self.dt * dot_orientation(self.orientation, Qdot[-3:])
+        self.position += self.dt * Qdot[:3]
+        self.orientation += self.dt * dot_orientation(self.orientation, Qdot[3:6])
         self.orientation = rescale_orientation(self.orientation)
+        self.dofs += self.dt * Qdot[6:]
 
         self.time += self.dt
-        self.trajectory.append([self.dof, self.position, self.orientation])
+        self.trajectory.append([self.position, self.orientation, self.dofs])
 
     def integrate_rk2(self):
         """Runge-Kutta 2nd order (Midpoint) integration."""
 
         # Step 1: Store current state
-        current_dof = self.dof.copy()
+        current_dof = self.dofs.copy()
         current_position = self.position.copy()
         current_orientation = self.orientation.copy()
 
         # First step (calculate k1)
         Qdot = self.grand_velocity()  # Compute the velocity
 
-        k1_dof = Qdot[:-6]
-        k1_position = Qdot[-6:-3]
-        k1_orientation = Qdot[-3:]
+        k1_position = Qdot[:3]
+        k1_orientation = Qdot[3:6]
+        k1_dof = Qdot[6:]
 
         # Second step (calculate k2), calculate the mid-point velocity (using the current values)
-        self.dof = current_dof + self.dt * k1_dof / 2
         self.position = current_position + self.dt * k1_position / 2
         self.orientation = current_orientation + self.dt * k1_orientation / 2
+        self.dofs = current_dof + self.dt * k1_dof / 2
 
         # Compute Qdot at the midpoint
         Qdot_mid = self.grand_velocity()  # Compute midpoint velocity
-        k2_dof = Qdot_mid[:-6]
-        k2_position = Qdot_mid[-6:-3]
-        k2_orientation = Qdot_mid[-3:]
+        k2_position = Qdot_mid[:3]
+        k2_orientation = Qdot_mid[3:6]
+        k2_dof = Qdot_mid[6:]
 
         # Update the state with weighted sum of k1 and k2
-        self.dof = current_dof + self.dt * (k1_dof + k2_dof) / 2
         self.position = current_position + self.dt * (k1_position + k2_position) / 2
         self.orientation = current_orientation + self.dt * (k1_orientation + k2_orientation) / 2
         self.orientation = rescale_orientation(self.orientation)  # Ensure orientation is valid
+        self.dofs = current_dof + self.dt * (k1_dof + k2_dof) / 2
 
         # Update time and store trajectory
         self.time += self.dt
-        self.trajectory.append([self.dof, self.position, self.orientation])
+        self.trajectory.append([self.position, self.orientation, self.dofs])
 
     def integrate_rk4(self):
         """Runge-Kutta 4th order integration."""
 
         # Step 1: Store current state
-        current_dof = self.dof.copy()
+        current_dof = self.dofs.copy()
         current_position = self.position.copy()
         current_orientation = self.orientation.copy()
 
         # First step (calculate k1)
         Qdot = self.grand_velocity()  # Compute the velocity
 
-        k1_dof = Qdot[:-6]
-        k1_position = Qdot[-6:-3]
-        k1_orientation = Qdot[-3:]
+        k1_position = Qdot[:3]
+        k1_orientation = Qdot[3:6]
+        k1_dof = Qdot[6:]
 
         # Second step (calculate k2)
-        self.dof = current_dof + self.dt * k1_dof / 2
+        self.dofs = current_dof + self.dt * k1_dof / 2
         self.position = current_position + self.dt * k1_position / 2
         self.orientation = current_orientation + self.dt * k1_orientation / 2
         Qdot_k2 = self.grand_velocity()
-        k2_dof = Qdot_k2[:-6]
-        k2_position = Qdot_k2[-6:-3]
-        k2_orientation = Qdot_k2[-3:]
+        k2_position = Qdot_k2[:3]
+        k2_orientation = Qdot_k2[3:6]
+        k2_dof = Qdot_k2[6:]
 
         # Third step (calculate k3)
-        self.dof = current_dof + self.dt * k2_dof / 2
+        self.dofs = current_dof + self.dt * k2_dof / 2
         self.position = current_position + self.dt * k2_position / 2
         self.orientation = current_orientation + self.dt * k2_orientation / 2
         Qdot_k3 = self.grand_velocity()
-        k3_dof = Qdot_k3[:-6]
-        k3_position = Qdot_k3[-6:-3]
-        k3_orientation = Qdot_k3[-3:]
+        k3_position = Qdot_k3[:3]
+        k3_orientation = Qdot_k3[3:6]
+        k3_dof = Qdot_k3[6:]
 
         # Fourth step (calculate k4)
-        self.dof = current_dof + self.dt * k3_dof
+        self.dofs = current_dof + self.dt * k3_dof
         self.position = current_position + self.dt * k3_position
         self.orientation = current_orientation + self.dt * k3_orientation
         Qdot_k4 = self.grand_velocity()
-        k4_dof = Qdot_k4[:-6]
-        k4_position = Qdot_k4[-6:-3]
-        k4_orientation = Qdot_k4[-3:]
+        k4_position = Qdot_k4[:3]
+        k4_orientation = Qdot_k4[3:6]
+        k4_dof = Qdot_k4[6:]
 
         # Update the state with weighted sum of k1, k2, k3, k4
-        self.dof = current_dof + self.dt * (k1_dof + 2 * k2_dof + 2 * k3_dof + k4_dof) / 6
         self.position = (
             current_position + self.dt * (k1_position + 2 * k2_position + 2 * k3_position + k4_position) / 6
         )
@@ -188,10 +193,11 @@ class FlowBody:
             + self.dt * (k1_orientation + 2 * k2_orientation + 2 * k3_orientation + k4_orientation) / 6
         )
         self.orientation = rescale_orientation(self.orientation)  # Ensure orientation is valid
+        self.dofs = current_dof + self.dt * (k1_dof + 2 * k2_dof + 2 * k3_dof + k4_dof) / 6
 
         # Update time and store trajectory
         self.time += self.dt
-        self.trajectory.append([self.dof, self.position, self.orientation])
+        self.trajectory.append([self.position, self.orientation, self.dofs])
 
     def simulate(self, T):
         """Run the simulation for time T."""
@@ -273,13 +279,13 @@ def _rotation_matrix_from_Rodrigues_impl(rvec, Ndof):
         K = jnp.array([[0, -kz, ky], [kz, 0, -kx], [-ky, kx, 0]])
         R = jnp.eye(3) + jnp.sin(theta) * K + (1 - jnp.cos(theta)) * jnp.dot(K, K)
 
-        grand_R = jnp.block(
+        sixc_R = jnp.block(
             [
-                [jnp.eye(Ndof), jnp.zeros((Ndof, 6))],
-                [jnp.zeros((3, Ndof)), R, jnp.zeros((3, 3))],
-                [jnp.zeros((3, Ndof)), jnp.zeros((3, 3)), R],
+                [R, jnp.zeros((3, 3)), jnp.zeros((3, Ndof))],  # pos
+                [jnp.zeros((3, 3)), R, jnp.zeros((3, Ndof))],  # ori
+                [jnp.zeros((Ndof, 6)), jnp.eye(Ndof)],  # dof
             ]
         )
-        return R, grand_R
+        return R, sixc_R
 
     return lax.cond(theta < 1e-6, no_rotation, compute_rotation, None)
